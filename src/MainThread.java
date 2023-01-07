8e5a638ff1fde705d47a63d9e1352ed6e362da6e
DLight <dlight7932@gmail.com>
2023-01-07 15:16:34 +0200

    commit

---
+++ src/MainThread.java
@@ -0,0 +1,95 @@
+import java.util.concurrent.CyclicBarrier;
+
+public class MainThread {
+    private long sum;
+    private int threadCount;
+    private int partCount;
+    private int size;
+    private int[] mas;
+    private boolean[] partsAreCalculated;
+    private CyclicBarrier bar;
+
+    MainThread(int[] mas_, int threadCount_, int partCount_)
+    {
+        mas = mas_;
+        size = mas.length;
+        threadCount = threadCount_;
+        partCount = partCount_;
+        sum = 0;
+        partsAreCalculated = new boolean[partCount];
+        for (int i = 0; i < threadCount; i++)
+        {
+            partsAreCalculated[i] = true;
+        }
+        bar = new CyclicBarrier(threadCount, new Runnable() {
+            @Override
+            public void run() {
+                WakeUp();
+            }
+        });
+    }
+
+    public long getSum()
+    {
+        return sum;
+    }
+    public long getPartCount()
+    {
+        return partCount;
+    }
+
+    synchronized public void Start(){
+        //thread creating
+        int neededThreadCount = Math.min(threadCount, partCount);
+        SumThread[] threadsOther = new SumThread[neededThreadCount];
+        Thread[] threads = new Thread[neededThreadCount];
+
+        //thread starting
+        for (int i = 0; i < neededThreadCount; i++) {
+            threadsOther[i] = new SumThread(this, mas, i);
+            threads[i] = new Thread(threadsOther[i]);
+            threads[i].start();
+        }
+
+        //waiting for threads to finish
+        try {
+            wait();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    synchronized public void setPartSum(long partSum){
+        sum = sum + partSum;
+    }
+
+    synchronized public int GetFreePart(){
+        int newPart = -1;
+        for (int i = partsAreCalculated.length - 1; i >= threadCount; i--)
+        {
+            if (!partsAreCalculated[i])
+            {
+                newPart = i;
+                partsAreCalculated[i] = true;
+                break;
+            }
+        }
+        return newPart;
+    }
+
+    public void AwaitBarrier()
+    {
+        try {
+            bar.await();
+        } catch (InterruptedException ex) {
+            return;
+        } catch (java.util.concurrent.BrokenBarrierException ex) {
+            return;
+        }
+    }
+    synchronized public void WakeUp()
+    {
+        notify();
+    }
+}
---
+++ src/SumThread.java
@@ -0,0 +1,33 @@
+import java.util.concurrent.CyclicBarrier;
+
+public class SumThread implements Runnable {
+    long partSum = 0;
+    int[] mas;
+    int begin, end;
+    int partNum;
+    MainThread mainThread;
+
+    SumThread(MainThread mainThread_, int[] mas, int partNum_) {
+        this.mas = mas;
+        mainThread = mainThread_;
+        partNum = partNum_;
+    }
+
+    @Override
+    public void run() {
+        int newPart = -1;
+        do {
+            partSum = 0;
+            begin = (int) (mas.length / mainThread.getPartCount() * partNum);
+            end = (int) (begin + mas.length / mainThread.getPartCount() - 1);
+            for (int i = begin; i <= end; i++) {
+                partSum = partSum + mas[i];
+            }
+            mainThread.setPartSum(partSum);
+            newPart = mainThread.GetFreePart();
+            partNum = newPart;
+        }
+        while (newPart != -1);
+        mainThread.AwaitBarrier();
+    }
+}
